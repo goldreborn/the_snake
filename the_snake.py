4b102b457e8ab87360b65e11feb5573f79351a8e
yakovkuznetsov <154214741+yakovkuznetsov@users.noreply.github.com>
2023-12-20 15:37:21 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -1,7 +1,8 @@
 
+from random import randint, choice
+
 import pygame
 
-from random import randint
 
 pygame.init()
 
@@ -18,13 +19,21 @@ APPLE_COLOR = (255, 0, 0)
 BG_COLOR = (0, 0, 0)
 SPEED = 10
 
-# Создаем класс гейм обджект 
+field = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Змейка')
+
+clock = pygame.time.Clock()
+
+# Создаем класс GameObject
+
 
 class GameObject:
 
     def __init__(self, position, body_color):
         self.position = position
         self.body_color = body_color
+
+    # создаём случайные координаты
 
     @staticmethod
     def random_axis(cell_size):
@@ -35,12 +44,17 @@ class GameObject:
         return x, y
 
 
+# Создаем класс Snake
+
+
 class Snake(GameObject):
 
     direction = 'right'
 
     def __init__(self, position, body_color):
         super().__init__(position, body_color)
+
+    # Функция движения змейки
 
     def move(self):
 
@@ -76,10 +90,15 @@ class Snake(GameObject):
             self.position.insert(0, (self.position[0][0], SCREEN_HEIGHT))
 
 
+# Создаем класс Apple
+
+
 class Apple(GameObject):
 
     def __init__(self, position, body_color):
         super().__init__(position, body_color)
+
+    # Определяем случайное место для яблока
 
     def randomize_position(self):
 
@@ -91,17 +110,8 @@ class Apple(GameObject):
         print('apple has been eaten')
 
 
-field = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Snake')
-
-clock = pygame.time.Clock()
-
 field.fill(BG_COLOR)
 
-snake = Snake([(FIELD_CENTER['x'], FIELD_CENTER['y'])], SNAKE_COLOR)
-apple_x, apple_y = GameObject.random_axis(CELL_SIZE)
-
-apple = Apple([(apple_x, apple_y)], APPLE_COLOR)
 
 
 def draw(field, color, axis):
@@ -110,27 +120,16 @@ def draw(field, color, axis):
 
 def main():
 
-    running = True
+    snake = Snake([(FIELD_CENTER['x'], FIELD_CENTER['y'])], SNAKE_COLOR)
 
-    while running:
+    apple_x, apple_y = GameObject.random_axis(CELL_SIZE)
+    apple = Apple([(apple_x, apple_y)], APPLE_COLOR)
+
+    while True:
 
         clock.tick(SPEED)
 
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and snake.direction != 'down':
-                    snake.direction = 'up'
-                elif event.key == pygame.K_DOWN and snake.direction != 'up':
-                    snake.direction = 'down'
-                elif event.key == pygame.K_LEFT and snake.direction != 'right':
-                    snake.direction = 'left'
-                elif event.key == pygame.K_RIGHT and snake.direction != 'left':
-                    snake.direction = 'right'
-                else:
-                    pass
+        handle_keys(snake)
 
         snake.move()
 
@@ -174,6 +173,24 @@ def main():
 
         print(snake.position)
         pygame.display.update()
+
+
+def handle_keys(game_object):
+    for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and game_object.direction != 'down':
+                    game_object.direction = 'up'
+                elif event.key == pygame.K_DOWN and game_object.direction != 'up':
+                    game_object.direction = 'down'
+                elif event.key == pygame.K_LEFT and game_object.direction != 'right':
+                    game_object.direction = 'left'
+                elif event.key == pygame.K_RIGHT and game_object.direction != 'left':
+                    game_object.direction = 'right'
+                else:
+                    pass
 
 
 main()
